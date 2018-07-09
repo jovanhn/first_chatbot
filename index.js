ebb14f00a3b96d81da64dd2f71e792753c12fca1
Jovan Suberic <suberic.jovan@gmail.com>
2018-07-09 11:11:01 +0200

    Message handling

--- index.js
+++ index.js
@@ -37,13 +37,25 @@ app.post('/webhook', (req, res) => {
   
       // Iterate over each entry - there may be multiple if batched
       body.entry.forEach(function(entry) {
-  
-        // Get the webhook event. entry.messaging is an array, but 
-        // will only ever contain one event, so we get index 0
+
+        // Gets the body of the webhook event
         let webhook_event = entry.messaging[0];
         console.log(webhook_event);
+      
+      
+        // Get the sender PSID
+        let sender_psid = webhook_event.sender.id;
+        console.log('Sender PSID: ' + sender_psid);
+      
+        // Check if the event is a message or postback and
+        // pass the event to the appropriate handler function
+        if (webhook_event.message) {
+          handleMessage(sender_psid, webhook_event.message);        
+        } else if (webhook_event.postback) {
+          handlePostback(sender_psid, webhook_event.postback);
+        }
         
-      });
+        });
   
       // Return a '200 OK' response to all events
       res.status(200).send('EVENT_RECEIVED');
@@ -59,17 +71,48 @@ app.post('/webhook', (req, res) => {
 
     let response;
   
-    // Check if the message contains text
-    if (received_message.text) {    
-  
-      // Create the payload for a basic text message
+    // Checks if the message contains text
+    if (received_message.text) {
+      
+      // Creates the payload for a basic text message, which
+      // will be added to the body of our request to the Send API
       response = {
-        "text": `You sent the message: "${received_message.text}". Now send me an image!`
+        "text": `You sent the message: "${received_message.text}". Now send me an attachment!`
       }
-    }  
+  
+    } else if (received_message.attachments) {
+    
+      // Gets the URL of the message attachment
+      let attachment_url = received_message.attachments[0].payload.url;
+    
+    } 
     
     // Sends the response message
     callSendAPI(sender_psid, response);    
+  }
+
+  function callSendAPI(sender_psid, response) {
+    // Construct the message body
+    let request_body = {
+      "recipient": {
+        "id": sender_psid
+      },
+      "message": response
+    }
+  
+    // Send the HTTP request to the Messenger Platform
+    request({
+      "uri": "https://graph.facebook.com/v2.6/me/messages",
+      "qs": { "access_token": FB_ACCESS_TOKEN },
+      "method": "POST",
+      "json": request_body
+    }, (err, res, body) => {
+      if (!err) {
+        console.log('message sent!')
+      } else {
+        console.error("Unable to send message:" + err);
+      }
+    }); 
   }
 
 app.listen(app.get('port'), function(){
