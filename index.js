5f9733507e9b4dadac408700e658d82fa34eb3ea
Jovan Suberic <suberic.jovan@gmail.com>
2018-07-09 10:58:40 +0200

    Post messagess index.js

--- index.js
+++ index.js
@@ -6,6 +6,9 @@ const request = require('request')
 
 const app = express()
 
+const token = process.env.FB_VERIFY_TOKEN
+const access = proces.env.FB_ACCESS_TOKEN
+
 app.set('port', (process.env.PORT || 5000))
 
 
@@ -18,11 +21,56 @@ app.get('/', function(req,res){
 })
 
 app.get('/webhook/', function(req,res){
-    if (req.query['hub.verify_token'] === "jovan_hn"){
+    if (req.query['hub.verify_token'] === token){
         res.send(req.query['hub.challenge'])
     }
     res.send("Wrong token")
 })
+
+app.post('/webhook', (req, res) => {  
+
+    // Parse the request body from the POST
+    let body = req.body;
+  
+    // Check the webhook event is from a Page subscription
+    if (body.object === 'page') {
+  
+      // Iterate over each entry - there may be multiple if batched
+      body.entry.forEach(function(entry) {
+  
+        // Get the webhook event. entry.messaging is an array, but 
+        // will only ever contain one event, so we get index 0
+        let webhook_event = entry.messaging[0];
+        console.log(webhook_event);
+        
+      });
+  
+      // Return a '200 OK' response to all events
+      res.status(200).send('EVENT_RECEIVED');
+  
+    } else {
+      // Return a '404 Not Found' if event is not from a page subscription
+      res.sendStatus(404);
+    }
+  
+  });
+
+  function handleMessage(sender_psid, received_message) {
+
+    let response;
+  
+    // Check if the message contains text
+    if (received_message.text) {    
+  
+      // Create the payload for a basic text message
+      response = {
+        "text": `You sent the message: "${received_message.text}". Now send me an image!`
+      }
+    }  
+    
+    // Sends the response message
+    callSendAPI(sender_psid, response);    
+  }
 
 app.listen(app.get('port'), function(){
     console.log("running: port")
